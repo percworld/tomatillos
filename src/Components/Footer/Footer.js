cf5a20d922adc29d7df506a0ad4a52e57c101e60
jgiwinski <juliaiwinski@gmail.com>
2021-03-26 17:27:29 -0700

    Add styling to title, tagline, and specs box. Added icons to specs

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer>Footer</footer>
+        <footer className='text'>Footer</footer>
     )
 }
 export default Footer;
--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './MovieDetails.css';
+import { FaClock } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 
 const MovieDetails = ({ film }) => {
 
     const { id, title, poster_path, average_rating, budget, genres, overview, release_date, revenue, runtime, tagline } = film;
-
+    const roundedRating = Math.floor( average_rating * 100) / 100
     return (
         <section className='movie-details-container'>
-            <div>
+            <div className='cover-container'>
                 <img className='featured-cover' src={poster_path} />
             </div>
             <div className='movie-descrip-container'>
                 <h1 className='title'>{title}</h1>
-                <h4>{tagline}</h4>
-                <div>
-                    <p>{average_rating}</p>
+                <h4 className='tagline'>"{tagline}"</h4>
+                <div className='row specs-box'>
+                    <FaStar />
+                    <p>{roundedRating}</p>
                     {/* <p>{genres}</p> */}
+                    <FaClock />
                     <p>{runtime} min</p>
                 </div>
-                <h5>{overview}</h5>
+                <h3 className='overview'>{overview}</h3>
                 <div>
                     <div>
                         <p>{budget}</p>
