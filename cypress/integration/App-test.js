ed2280fde05f8ebfc68f1e7bef871c250c9ba9c0
percworld <percworld@gmail.com>
2021-03-31 23:37:41 -0600

    Add testing init

--- cypress/integration/App-test.js
+++ cypress/integration/App-test.js
@@ -1,1 +1,51 @@
+describe('response check', () => {
+    // it('should get movie data upon fetch', () => {
+    //     cy.intercept({
+    //         method: 'GET',
+    //         url: 'http//localhost:3000'
+    //     },
+    //         {
+    //             body: "Here is a new string"
+    //         }).as('apiCheck')
+    //     cy.wait('@apiCheck').then((interception) => {
+    //         assert.isNotNull(interception.response.body, '1st API call has data')
+    //     })
+    // })
+})
+describe('As a user', () => {
+    beforeEach('go to site', () => {
+        cy.visit('http://localhost:3000/')
+    })
+    it('displays the home page', () => {
+        cy.get("header").contains("h4", "RANCID")
+            .get('.gridDisplay')
+    })
 
+    it('executes a movie search', () => {
+        cy.get('input[type=text]').type('rogue')
+            .should('have.value', 'rogue')
+            .get(".submit-btn").click()
+        cy.get(".mainCoverImage").click()
+        cy.url().should('include', '/')
+    })
+
+    it('can display movie details', () => {
+        cy.get('input[type=text]').type('mulan')
+            .get(".submit-btn").click()
+        cy.get(".mainCoverImage").click()
+        cy.url().should('include', '/337401')
+        cy.get('.small-specs-box').contains('h4', "September")
+        cy.get('.specs-box').contains('5')
+    })
+
+    it('should go back to main view by clicking go Back button', () => {
+        cy.get('input[type=text]').type('peninsula')
+            .get(".submit-btn").click()
+        cy.get(".mainCoverImage").click()
+        cy.url().should('include', '/581392')
+        cy.get('.backArrow').click()
+        cy.url().should('include', '/')
+
+    })
+
+})
